f1ed3de2c93bade19ce97699bf17a7e19262b70a
osama drbas <osamadrbas31@gmail.com>
2025-09-11 18:21:51 +0300

    Create: created requirements file

--- newsApi/settings.py
+++ newsApi/settings.py
@@ -24,9 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n%*2^_2pp^s!yk%^v$$x)q4-l(6+9#c)vnb*xf=p@dvd%xf7qx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['od317.pythonanywhere.com', 'localhost', '127.0.0.1']
+DEBUG = False  # Set to False for production
 
 
 # Application definition
@@ -160,6 +159,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
